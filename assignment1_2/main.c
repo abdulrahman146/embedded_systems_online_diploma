5e9284a7c0649098ef1ef9894e16a07d34f7a2cc
abdulrahman <ab.yaser88@gmail.com>
2020-11-22 02:33:01 +0200

    2

---
+++ assignment1_2/main.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : assignment1_2.c
+ Author      : abdulrahman yasser
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int x;
+	fflush(stdin); fflush(stdout);
+		printf("Enter an integer: ");
+		fflush(stdin); fflush(stdout);
+		scanf("%d",&x);
+		printf("You enterd %d",x);
+	return 0;
+}
